e22c77c9cbfb25c0aed2e99ff43870aa1839c6aa
vickyimagine <vivek@beimagine.tech>
2023-04-04 14:12:50 +0530

    URI file integrated

---
+++ src/utils/URI.js
@@ -0,0 +1,22 @@
+export const uriList = [
+  {
+    id: 0,
+    descrp: "Enrolled",
+    uri: "https://gateway.pinata.cloud/ipfs/QmWUTVYaeTSFQeHAqUGXSM9UEdGg1256DWxZQ6wwD84HaE",
+  },
+  {
+    id: 1,
+    descrp: "PHASE 1",
+    uri: "https://gateway.pinata.cloud/ipfs/QmacRcgZu8cNob9tJfr6NFmz7KKeANTFXRxA9P6RA3B2Q8",
+  },
+  {
+    id: 2,
+    descrp: "PHASE 2",
+    uri: "https://gateway.pinata.cloud/ipfs/QmRhk6sQiREpu2wSVpCi3SDRAYi7g18fReiT9hVK5MMYrz",
+  },
+  {
+    id: 3,
+    descrp: "COMPLETED",
+    uri: "https://gateway.pinata.cloud/ipfs/QmUFZXKcrgFMeTskAR5ys5mchojFGsvyCq9BRDdVpZJ4NE",
+  },
+];
